eb954d02e58afe8b89b7718527718f633aff382a
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-11-13 13:12:51 +0000

    Add finished lab

---
+++ Bus.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace bus_lab
+{
+    public class Bus
+    {
+
+        public int Number { get; set; }
+        public string Destination { get; set; }
+        public Person[] Passengers { get; set; }
+         
+        public Bus(int number, string destination){
+            this.Number = number;
+            this.Destination = destination;
+            this.Passengers = new Person[20];
+        }
+
+        public string Drive(){
+            return "Brum brum!";
+        }
+
+        public int GetNumberOfPassengers(){
+            int counter = 0;
+            foreach(var person in this.Passengers){
+                if(person != null){
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public bool IsFull()
+        {
+            return this.GetNumberOfPassengers() == this.Passengers.Length;
+        }
+
+        public void PickUp(Person passenger)
+        {
+            if (this.IsFull())
+            {
+                return;
+            }
+
+            int passengerCount = this.GetNumberOfPassengers();
+            this.Passengers[passengerCount] = passenger;
+        }
+
+        public void DropOff(Person passenger)
+        {
+            for (var i = 0; i < this.Passengers.Length; i++)
+            {
+                if(this.Passengers[i] == passenger)
+                {
+                    this.Passengers[i] = null;
+                }
+            }
+        }
+
+        public void Empty()
+        {
+            for (var i = 0; i < this.Passengers.Length; i++)
+            {
+                this.Passengers[i] = null;
+            }
+        }
+ 
+    }
+}
+
---
+++ Test.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System;
+namespace bus_lab
+{
+    [TestFixture()]
+    public class BusTest
+    {
+        private Bus bus1;
+        private Person person1;
+
+        [SetUp]
+        public void Init()
+        {
+            bus1 = new Bus(22, "Ocean Terminal"); 
+            person1 = new Person("Gaby", 27);
+        }
+
+        [Test()]
+        public void TestBusHasNumber()
+        {
+            Assert.AreEqual(22, bus1.Number);
+        }
+
+        [Test()]
+        public void TestCanSetBusNumber()
+        {
+            bus1.Number = 11;
+            Assert.AreEqual(11, bus1.Number);
+        }
+
+        [Test()]
+        public void TestBusHasDestination()
+        {
+            Assert.AreEqual("Ocean Terminal", bus1.Destination);
+        }
+
+        [Test()]
+        public void TestBusCanDrive()
+        {
+            Assert.AreEqual("Brum brum!", bus1.Drive());
+        }
+
+        [Test()]
+        public void TestPersonHasName()
+        {
+            Assert.AreEqual("Gaby", person1.Name);
+        }
+
+        [Test()]
+        public void TestPersonHasAge()
+        {
+            Assert.AreEqual(27, person1.Age);
+        }
+
+        [Test()]
+        public void TestBusHasPassengersArray()
+        {
+            Assert.AreEqual(20, bus1.Passengers.Length);
+        }
+
+        [Test()]
+        public void TestBusStartsEmpty()
+        {
+            Assert.AreEqual(0, bus1.GetNumberOfPassengers());
+        }
+
+        [Test()]
+        public void TestBusCanPickUpPassengers()
+        {
+            bus1.PickUp(person1);
+            Assert.AreEqual(1, bus1.GetNumberOfPassengers());
+            Assert.AreEqual(person1, bus1.Passengers[0]);
+        }
+
+        [Test()]
+        public void TestBusCanDropOffPassengers()
+        {
+            bus1.PickUp(person1);
+            bus1.DropOff(person1);
+            Assert.AreEqual(0, bus1.GetNumberOfPassengers());
+        }
+
+        [Test()]
+        public void TestCanEmptyBus()
+        {
+            bus1.PickUp(person1);
+            bus1.Empty();
+            Assert.AreEqual(0, bus1.GetNumberOfPassengers());
+        }
+    }
+}
